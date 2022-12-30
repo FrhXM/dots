316d64af94268838aac13828f80e17e305b8595c
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-12-30 22:08:11 +0100

    Add files via upload

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -1,1 +1,15 @@
-
+#!/usr/bin/sh
+# APP DEMON #
+#-----------#
+/usr/bin/unclutter &
+/usr/bin/udiskie &
+/usr/bin/dunst &
+/usr/bin/picom &
+# SYSTEMTRAY #
+#------------#
+/usr/bin/blueman-applet &
+/usr/bin/nm-applet &
+# OTHERS #
+#--------#
+xwallpaper --zoom ~/pix/wall/anime.png &
+xset r rate 200 80 &
