15250d620c6614a45a0372f7fd43aa87467622d3
Farah^_^,Abderrazzak <108276410+FrhXM@users.noreply.github.com>
2022-12-30 22:13:04 +0100

    Add files via upload

--- .config/rofi/scripts/powerMenu.sh
+++ .config/rofi/scripts/powerMenu.sh
@@ -1,1 +1,10 @@
+#! /bin/sh
 
+chosen=$(printf "  Power Off\n  Restart\n  Lock" | rofi -dmenu -i -theme-str '@import "power.rasi"' -p " Good Bye My Friend  ")
+
+case "$chosen" in
+	"  Power Off") systemctl poweroff ;;
+	"  Restart") systemctl reboot ;;
+	"  Lock") slock ;;
+	*) exit 1 ;;
+esac
